183c48c14cdfb4ecc4837dc67dfe55bffb33d49a
Daiki <nojiri1098@gmail.com>
2019-09-21 12:43:27 +0900

    Add package/nojiri1098/laravel-multiauth

---
+++ package/nojiri1098/laravel-multiauth/src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Nojiri1098\LaravelMultiAuth;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        dump('ok');
+    }
+}
