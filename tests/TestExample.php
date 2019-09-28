7019cb5514d81218bfd63001b2128a0424d5ac7b
Daiki <nojiri1098@gmail.com>
2019-09-28 12:38:20 +0900

    Add test

---
+++ tests/TestExample.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class ExampleTest
+ * @package ExampleTest
+ */
+class ExampleTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @test
+     */
+    public function テスト()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
